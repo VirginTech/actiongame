be94f158a8ec02ad367e78af59d0462e55c38516
OOTANI,Kenji <ootani@virgintech.co.jp>
2016-04-02 11:56:35 +0900

    Mainシーン作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Token {
+
+	[SerializeField]
+	float _RunSpeed=2;
+	[SerializeField]
+	float _JumpSpeed=4;
+
+	void Update(){
+
+		Vector2 v = Util.GetInputVector ();
+		VX = v.x * _RunSpeed;
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			VY = _JumpSpeed;
+		}
+	}
+
+}
